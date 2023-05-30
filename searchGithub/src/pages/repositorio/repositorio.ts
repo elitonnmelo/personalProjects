e9567f15d8e2d5e6ec47337ba076c059a8fe811d
elitonnmelo <elitonp.melo@gmail.com>
2023-05-30 20:32:08 -0300

    adding  generic page

---
+++ searchGithub/src/pages/repositorio/repositorio.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the RepositorioPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-repositorio',
+  templateUrl: 'repositorio.html',
+})
+export class RepositorioPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RepositorioPage');
+  }
+
+}
