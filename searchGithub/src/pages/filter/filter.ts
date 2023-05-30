c94b1fae09d71a385055e320e2521b38142dfea2
elitonnmelo <elitonp.melo@gmail.com>
2023-05-30 19:50:25 -0300

    updating ts filter

--- searchGithub/src/pages/filter/filter.ts
+++ searchGithub/src/pages/filter/filter.ts
@@ -14,10 +14,51 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'filter.html',
 })
 export class FilterPage {
+  value = [];
+
+  language = [
+    {
+      nome: "Python"
+    },{
+      nome: "JavaScript"
+    },{
+      nome: 'TypeScript'
+    },{
+      nome: "HTML"
+    },{
+      nome: "CSS"
+    },{
+      nome: "Java"
+    },{
+      nome: "C#"
+    },{
+      nome: "PHP"
+    },{
+      nome: "Ruby"
+    },{
+      nome: "Swift"
+    },{
+      nome: "Kotlin"
+    },{
+      nome: "Go"
+    },{
+      nome: "Scala"
+    },{
+      nome: "Rust"
+    }
+  ]
+
+  currentPage = 1;
+  pageSize = 2;
+  paginatedItems: any[] = [];
+  totalPages: number;
+  //searchTerm: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public viewCtrl: ViewController
     ) {
+      this.totalPages = Math.ceil(this.language.length / this.pageSize); 
+      this.paginatedItems = this.language.slice(0, this.pageSize); 
   }
 
   ionViewDidLoad() {
@@ -27,4 +68,24 @@ export class FilterPage {
     this.viewCtrl.dismiss();
   }
 
+  nextPage() {
+    if (this.currentPage * this.pageSize < this.language.length) {
+      this.currentPage++;
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+
+      // Atualiza a lista paginada com os itens da nova página
+      this.paginatedItems = this.language.slice(startIndex, endIndex);
+    }
+  }
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+
+      this.paginatedItems = this.language.slice(startIndex, endIndex);
+    }
+  }
+  
 }
